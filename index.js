a433f5469968f2d81ac84bbf2dab9698c65a8bcb
Madeleine Heineman <mheineman16@gmail.com>
2022-09-08 14:02:52 -0400

    commit tree 754d14340a491464a61ec42a2e9ae1cf01039b3a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    const newText = document.querySelector('#text')
+    newText.textContent = "This is really cool!"
+})
+
